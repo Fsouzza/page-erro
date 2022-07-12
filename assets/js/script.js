cdd527213ad68f780b23a049d4a1f9692205d3e3
F. Souzza <filipesouzza.adm@gmail.com>
2022-07-11 23:43:52 -0300

    pagina de erro 404

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+/* =========== SHOW MENU ========== */
+const menu = document.getElementById("nav-menu"), 
+    navToggle = document.getElementById("nav-toggle"), 
+    closeMenu = document.getElementById("nav-close")
+
+
+/* =========== MENU ========== */
+if (navToggle){
+    navToggle.addEventListener('click', () => {
+        menu.classList.add('show-menu');
+    });
+}
+
+
+/* =========== CLOSE MENU ========== */
+if (closeMenu){
+    closeMenu.addEventListener('click', () => {
+        menu.classList.remove('show-menu');
+    });
+};
+
+
+
+/* =========== REMOVER MENU MOBILE ========== */
+const navLink = document.getElementById('nav__link')
+
+function acaoLink(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', acaoLink))

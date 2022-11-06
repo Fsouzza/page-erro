08f87e76af0bca03102b0bb40cb5b1017bd16763
F. Souzza <filipesouzza.adm@gmail.com>
2022-11-05 22:25:52 -0300

    correcao de identacao

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,28 @@
 /* =========== SHOW MENU ========== */
 const menu = document.getElementById("nav-menu"), 
-    navToggle = document.getElementById("nav-toggle"), 
-    closeMenu = document.getElementById("nav-close")
-
+navToggle = document.getElementById("nav-toggle"), 
+closeMenu = document.getElementById("nav-close")
 
 /* =========== MENU ========== */
 if (navToggle){
-    navToggle.addEventListener('click', () => {
-        menu.classList.add('show-menu');
-    });
+  navToggle.addEventListener('click', () => {
+    menu.classList.add('show-menu');
+  });
 }
-
 
 /* =========== CLOSE MENU ========== */
 if (closeMenu){
-    closeMenu.addEventListener('click', () => {
-        menu.classList.remove('show-menu');
-    });
+  closeMenu.addEventListener('click', () => {
+    menu.classList.remove('show-menu');
+  });
 };
-
-
 
 /* =========== REMOVER MENU MOBILE ========== */
 const navLink = document.getElementById('nav__link')
 
 function acaoLink(){
-    const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show-menu')
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show-menu')
 }
 
 navLink.forEach(n => n.addEventListener('click', acaoLink))
